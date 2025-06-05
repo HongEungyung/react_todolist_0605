34255da810ba0b597d8bc9b440ce6b379f48e303
Hong Eungyung <eungyeong217@gmail.com>
2025-06-05 10:09:25 +0900

    css 초기화

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TodoList from './TodoList';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <>
+  <App />
+  <TodoList />
+  </>
 );
 
